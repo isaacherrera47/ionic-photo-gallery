fae60894949b0703f572d1ebb7e1a089f6079503
Isaac Herrera <isaacherrera47@gmail.com>
2020-04-13 14:46:50 -0500

    +Initial commit.

---
+++ src/pages/Tab2.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import {
+  IonCol,
+  IonContent,
+  IonFab,
+  IonFabButton,
+  IonGrid,
+  IonHeader,
+  IonIcon, IonImg,
+  IonPage, IonRow,
+  IonTitle,
+  IonToolbar
+} from '@ionic/react';
+import './Tab2.css';
+import {camera} from "ionicons/icons";
+import {usePhotoGallery} from "../hooks/usePhotoGallery";
+
+const Tab2: React.FC = () => {
+  const {takePhoto, photos} = usePhotoGallery();
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Photo Gallery</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Tab 2</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonGrid>
+          <IonRow>
+            {photos.map((photo, index) => (
+              <IonCol size="6" key={index}>
+                <IonImg src={photo.base64 ?? photo.webviewPath}/>
+              </IonCol>
+            ))}
+          </IonRow>
+        </IonGrid>
+        <IonFab vertical="bottom" horizontal="center" slot="fixed">
+          <IonFabButton onClick={() => takePhoto()}>
+            <IonIcon icon={camera}/>
+          </IonFabButton>
+        </IonFab>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab2;
